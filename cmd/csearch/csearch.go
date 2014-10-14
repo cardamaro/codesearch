62dd69c1bb9ae064522bd4760bc74457050cd7a6
Tim Falzone <tim@flzn.us>
2014-10-14 09:40:11 -0400

    changed import paths to this repo

--- cmd/csearch/csearch.go
+++ cmd/csearch/csearch.go
@@ -11,8 +11,8 @@ import (
 	"os"
 	"runtime/pprof"
 
-	"code.google.com/p/codesearch/index"
-	"code.google.com/p/codesearch/regexp"
+	"github.com/cardamaro/codesearch/index"
+	"github.com/cardamaro/codesearch/regexp"
 )
 
 var usageMessage = `usage: csearch [-c] [-f fileregexp] [-h] [-i] [-l] [-n] regexp
